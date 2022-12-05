3fbc44dfdfd0aed71f466d57a5ea153f060f0c31
grady1080 <sketchypea@hotmail.com>
2022-12-05 20:01:43 +0000

    day 01 in php

---
+++ day_01/php/day_01.php
@@ -0,0 +1,30 @@
+<?php
+//GET INPUT
+$input = file_get_contents('input_01.txt');
+
+//EXPLODE ON DOUBLE NEW LINE
+$elves = explode("\r\n\r\n",$input);
+
+//INITIALIZE TOTALS
+$totals = array();
+
+//TRAVERSE ELVES
+foreach($elves as $k=>$v){
+	//EXPLODE ON NEW LINE AND STORE
+	$elves[$k] = explode("\r\n",$v);
+	//STORE SUM IN TOTALS
+	$totals[$k] = array_sum($elves[$k]);
+}
+
+//GET MAX VALUE
+$maxValue = max($totals);
+
+//PART 1 ANSWER
+echo $maxValue . ' is the highest carried by a single elf';
+echo "\n";
+
+//SORT TOTALS
+arsort($totals);
+
+//
+echo array_sum(array_slice($totals,0,3,true)) . ' is the combined total of the top 3 elves';
