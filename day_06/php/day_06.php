8fd4be6377df37fbb5a3e6bb564047b843765992
grady1080 <sketchypea@hotmail.com>
2022-12-10 10:19:54 +0000

    simplified code

--- day_06/php/day_06.php
+++ day_06/php/day_06.php
@@ -1,27 +1,22 @@
 <?php
-
-function filter($i){
-	return $i === 1;
-}
-
 const START_OF_PACKET_MARKER_LENGTH = 4;
 const START_OF_MESSAGE_MARKER_LENGTH = 14;
 
 $input = str_split(file_get_contents('input.txt'));
 $buffer = array();
 $i = 0;
-//$l = START_OF_PACKET_MARKER_LENGTH;
-$l = START_OF_MESSAGE_MARKER_LENGTH;
+$l = START_OF_PACKET_MARKER_LENGTH;
+//$l = START_OF_MESSAGE_MARKER_LENGTH;
 
 while($letter = array_shift($input)){
 	$i++;
 	$buffer[] = $letter;
 	
-	if(count($buffer) == $l + 1){
-		array_shift($buffer);
-			
-		if(count(array_filter(array_count_values($buffer),'filter')) == $l){
+	if(count($buffer) == $l){
+		if(count(array_unique($buffer)) == $l){
 			break;
+		}else{
+			array_shift($buffer);
 		}
 	}
 }
